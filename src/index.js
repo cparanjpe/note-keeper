08bae1984828596131f5f7609737ba5c3ba61082
Chaitanya Paranjpe <112272151+cparanjpe@users.noreply.github.com>
2023-02-04 21:19:28 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
+
+
+//Challenge. Render all the notes inside notes.js as a seperate Note
+//component.
